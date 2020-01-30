a16a04b26880a3d99a12005addaada0747791da5
Jack Pertschuk <jp954@cornell.edu>
2020-01-29 23:07:14 -0800

    .

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,10 +1,18 @@
 if (window.location.pathname === '/demo/') {
   (function () {
     window.onload = function () {
-      const frame = this.document.getElementById('answerbot-iframe')
+      const frame = this.document.createElement('iframe')
       const urlParams = new URLSearchParams(window.location.search)
       frame.account_id = urlParams.get('account_id')
       frame.src = 'https://' + urlParams.get('site')
+      frame.is = 'x-frame-bypass'
+      frame.id = 'answerbot-iframe'
+      frame.style.position = 'absolute'
+      frame.style.left = '0px'
+      frame.style.border = 'none'
+      frame.style.width = '100%'
+      frame.style.height = '100%'
+      this.document.body.appendChild(frame)
     }
   })()
 }
