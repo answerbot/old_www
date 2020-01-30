d30572ab618f6b932bded38b615f293313b48965
Jack Pertschuk <jp954@cornell.edu>
2020-01-29 22:53:13 -0800

    .

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,8 +1,10 @@
-(function () {
-  window.onload = function () {
-    const frame = this.document.getElementById('answerbot-iframe')
-    const urlParams = new URLSearchParams(window.location.search)
-    frame.account_id = urlParams.get('account_id')
-    frame.src = urlParams.get('site')
-  }
-})()
+if (window.location.pathname === '/demo/') {
+  (function () {
+    window.onload = function () {
+      const frame = this.document.getElementById('answerbot-iframe')
+      const urlParams = new URLSearchParams(window.location.search)
+      frame.account_id = urlParams.get('account_id')
+      frame.src = urlParams.get('site')
+    }
+  })()
+}
