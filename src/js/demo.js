bc3bf027a3f801f638ba289a08692b2ee9e513ab
Jack Pertschuk <jp954@cornell.edu>
2020-01-31 18:30:29 -0800

    .

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,10 +1,11 @@
 if (window.location.pathname === '/demo/') {
+  const account_id = urlParams.get('account_id')
   (function () {
     window.onload = function () {
       const FrameElem = window.customElements.get('x-frame-bypass')
       const frame = new FrameElem()
       const urlParams = new URLSearchParams(window.location.search)
-      frame.account_id = urlParams.get('account_id')
+      frame.account_id = account_id
       frame.src = 'https://' + urlParams.get('site')
       frame.id = 'answerbot-iframe'
       frame.style.position = 'absolute'
